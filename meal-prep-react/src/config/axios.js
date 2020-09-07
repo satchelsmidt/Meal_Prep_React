1d9288f2c510301400570ec1d8da293d2d5d1c70
satchelsmidt <satchelsmidt@gmail.com>
2020-09-07 11:01:20 -0700

    changing axios urls

--- meal-prep-react/src/config/axios.js
+++ meal-prep-react/src/config/axios.js
@@ -8,19 +8,19 @@ export const axiosFoodInstance = axios.create({
 //TODO: configure dev environment to auto pull correct axios url for requests
 export const axiosAuthInstance = axios.create({
     // baseURL: "http://localhost:8080/api/auth",
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "https://polar-basin-68400.herokuapp.com/api/auth",
     timeout: 50000,
     withCredentials: true
 });
 
 export const axiosPlansInstance = axios.create({
     // baseURL: "http://localhost:8080/api/plans",
-    baseURL: "http://localhost:3000/api/plans",
+    baseURL: "https://polar-basin-68400.herokuapp.com/api/plans",
     timeout: 50000,
 });
 
 export const axiosRecipesInstance = axios.create({
     // baseURL: "http://localhost:8080/api/recipes",
-    baseURL: "http://localhost:3000/api/recipes",
+    baseURL: "https://polar-basin-68400.herokuapp.com/api/recipes",
     timeout: 50000,
 });
